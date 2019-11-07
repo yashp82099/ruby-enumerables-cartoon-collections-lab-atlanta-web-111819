12320e38d764807d717cb35831b914ba565c6021
yash patel <yash82099@yahoo.com>
2019-11-07 22:06:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  roll_call_dwarves
+  put
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  summon_captain_planet
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+  long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
